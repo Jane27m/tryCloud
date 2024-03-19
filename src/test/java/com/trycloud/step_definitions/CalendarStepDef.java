590fb4fbf95d298b97ca6e786429d123aad6b592
Aleksandar <kaslu1992@gmail.com>
2024-03-19 13:07:58 +0100

    Calendar page/stepdef updated

--- src/test/java/com/trycloud/step_definitions/CalendarStepDef.java
+++ src/test/java/com/trycloud/step_definitions/CalendarStepDef.java
@@ -1,4 +1,60 @@
 package com.trycloud.step_definitions;
 
+import com.trycloud.pages.CalendarPage;
+import com.trycloud.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class CalendarStepDef {
+
+    CalendarPage calendarPage = new CalendarPage();
+
+    @Given("User is on the calendar page")
+    public void userIsOnCalendarPage() {
+
+
+    }
+
+    @When("User selects the daily view")
+    public void userSelectsDailyView() {
+
+
+    }
+
+    @Then("Daily calendar view is displayed")
+    public void dailyCalendarViewIsDisplayed() {
+
+    }
+
+    @When("User selects the weekly view")
+    public void userSelectsWeeklyView() {
+
+    }
+
+    @Then("Weekly calendar view is displayed")
+    public void weeklyCalendarViewIsDisplayed() {
+
+    }
+
+    @When("User selects the monthly view")
+    public void userSelectsMonthlyView() {
+
+    }
+
+    @Then("Monthly calendar view is displayed")
+    public void monthlyCalendarViewIsDisplayed() {
+
+    }
+
+    @When("User creates a new event")
+    public void userCreatesNewEvent() {
+
+    }
+
+    @Then("Event is displayed on the monthly calendar view")
+    public void eventIsDisplayedOnMonthlyView() {
+
+    }
+
 }
---
+++ src/test/resources/features/Calendar_Aleksandar.feature
@@ -0,0 +1,8 @@
+Feature: Calendar Module Functionality
+
+  Scenario: Display Daily Calendar View
+
+    Given User is on the calendar page
+    When User selects the daily view
+    Then Daily calendar view is displayed
+
