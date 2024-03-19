3815b637ebc4e37c72374074d2d544cff139d746
Lisa <dyatka11@gmail.com>
2024-03-19 14:27:56 -0500

    Two scenarios are added

--- src/test/java/com/trycloud/pages/FilesPage.java
+++ src/test/java/com/trycloud/pages/FilesPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class FilesPage {
     public FilesPage() {
         PageFactory.initElements(Driver.getDriver(), this);
@@ -13,7 +15,9 @@ public class FilesPage {
     @FindBy(xpath = "//span[@class='icon icon-add']")
     public WebElement addButton;
 
-    @FindBy(xpath = "//span[normalize-space()='Upload file']")
+
+    //@FindBy(xpath = "//label[@data-action='upload']")
+    @FindBy(css = "input[type='file']")
     public WebElement uploadFileButton;
 
     @FindBy(xpath = "//span[@class='displayname'][normalize-space()='New folder']")
@@ -22,8 +26,11 @@ public class FilesPage {
     @FindBy(xpath = "//tr[@class='ui-droppable highlighted']//span[@class='icon icon-more']")
     public WebElement moreIcon;
 
-    @FindBy(xpath = "(//span[@class='icon icon-delete'])[2]")
+    @FindBy(xpath = " //span[@class='innernametext']")
+    public List<WebElement> uploadedFiles;
+
+    @FindBy(xpath = "//span[text()='Screen']/../../span/a[2]")
+    public WebElement threeDotsMenu;
+    @FindBy(xpath = "//a[@data-action='Delete']")
     public WebElement deleteButton;
-
-
 }
