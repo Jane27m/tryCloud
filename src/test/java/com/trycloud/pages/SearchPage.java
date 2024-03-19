346947cf498fd6210a343a980bf6952b835bd07c
ZulfiyaCydeo <ztazriyeva@gmail.com>
2024-03-19 10:40:50 -0400

    search page

---
+++ src/test/java/com/trycloud/pages/SearchPage.java
@@ -0,0 +1,9 @@
+package com.trycloud.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchPage {
+@FindBy(className = "material-design-icon__svg")
+    public WebElement searchButton;
+}
